4cdaa61d9013e99dfec439ba402fa1a97f5451a2
rushikeshdevlekar <96769979+rushikeshdevlekar@users.noreply.github.com>
2023-11-09 11:47:30 +0000

    op

--- 01_basics/03_conversionnOperation.js
+++ 01_basics/03_conversionnOperation.js
@@ -1,10 +1,10 @@
 let score = "Rushikesh";
-console.log(typeof score);
-console.log(typeof(score));
+//console.log(typeof score);
+//console.log(typeof(score));
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
 
 // "33" => 33
 // "33afa" => NaN
@@ -12,7 +12,7 @@ console.log(valueInNumber);
 
 let isLoggedIn = "rushikesh";
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -21,5 +21,44 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 33;
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+// **************** Operation ************ //
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello";
+let str2 = " Rushikesh";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log(1 + 2 + "2");
+
+// console.log((3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+""); 
+
+let num1, num2, num3;
+nu1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
+
+
+// https://tc39.es/ecma262/#sec-abstract-operations
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
