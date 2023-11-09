296c763e194a72f67d07082387c97fbf6b474d0b
rushikeshdevlekar <96769979+rushikeshdevlekar@users.noreply.github.com>
2023-11-09 10:52:09 +0000

    add

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 12345;
+let accountEmail = "test@gmail.com";
+var accountPassword = "12334";
+accountCity = "Mumbai";
+let accountState = "";
+
+//accountId = 2; //not allowed
+accountEmail = "dd@gmail.com";
+accountPassword = "0000";
+accountCity = "MH";
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountId,accountPassword,accountCity,accountState]);
