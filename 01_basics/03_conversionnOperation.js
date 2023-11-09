b48dfadb9dd39450c889fcf859f6b3ff2ed65343
rushikeshdevlekar <96769979+rushikeshdevlekar@users.noreply.github.com>
2023-11-09 11:27:45 +0000

    connversio of data type

---
+++ 01_basics/03_conversionnOperation.js
@@ -0,0 +1,25 @@
+let score = "Rushikesh";
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33afa" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "rushikesh";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "abc" => true
+
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
